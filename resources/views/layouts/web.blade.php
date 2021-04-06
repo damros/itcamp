56ed8599d81e87c01ab8523dc35fda9a0607dadc
Damian <damrosgh@gmail.com>
2021-04-06 01:23:37 -0300

    pequenio fix a jquery

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -14,7 +14,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -33,7 +33,7 @@
     </div>
 
     <!--Custom JavaScript -->
-    <script src="js/custom.js"></script>    
+    <script src="js/custom.min.js"></script>    
 
     @yield('scripts')
 
